e6350ba4f4e6b5bfb4996fe43526dea38008dd11
Joshua D <joshuadelapenha@yahoo.com>
2020-08-17 04:43:47 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    self.class.all << self 
+  end
+  
+  def self.all 
+    @@all
+  end
+  
+  def self.clear_all
+    self.all.clear
+  end 
+  
+  def self.print_all
+    self.all.map{|dog| dog.name}
+  end
+  
+end 
